87467c4a99a35a8116afeab0fcccc1b05d185c77
jyothika28 <jyothikajalla032@gmail.com>
2023-12-04 14:31:42 -0500

    last to final

--- src/users/client.js
+++ src/users/client.js
@@ -17,7 +17,7 @@ export const account = async () => {
     const response = await request.put(`${USERS_API}/${user._id}`, user);
     return response.data;
   };
-  //change
+  //chan
   export const findAllUsers = async () => {
     const response = await request.get(`${USERS_API}`);
     return response.data;
